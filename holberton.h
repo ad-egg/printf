32e3392a6a7916ca6755e80972f0113f30f57947
Andrew Lindburg <atlindburg@gmail.com>
2018-10-21 23:09:22 +0000

    Added struct that matches a specifier to its respective function

--- holberton.h
+++ holberton.h
@@ -8,9 +8,14 @@ void c_spec(va_list);
 void s_spec(va_list);
 void i_spec(va_list);
 void d_spec(va_list);
+/**
+ * struct spec_finder - Stores format specifier and respective func
+ * @spec: Format sepcifier
+ * @f: Function that handles specifier
+ */
 typedef struct spec_finder
 {
-	char *c;
+	char *spec;
 	void (*f)(va_list);
 } find_t;
 #endif
