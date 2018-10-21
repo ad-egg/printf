9e3781ec56312ce8565be1f6fa335f2f71bbe60d
Andrew Lindburg <atlindburg@gmail.com>
2018-10-21 22:33:25 +0000

    Added a bunch of function prototypes... consistent with the general architecture we laid down for printf

--- holberton.h
+++ holberton.h
@@ -4,4 +4,13 @@
 #include <stdarg.h>
 int _printf(const char *format, ...);
 unsigned int find_buffer_size(const char *str);
+void c_spec(va_list);
+void s_spec(va_list);
+void i_spec(va_list);
+void d_spec(va_list);
+typedef struct spec_finder
+{
+	char *c;
+	void (*f)(va_list);
+} find_t;
 #endif
