37deb4407ca4a6402bdd4429bfd6ec01d4d93e20
Andrew Lindburg <atlindburg@gmail.com>
2018-10-22 20:47:15 +0000

    Fixed compiler errors for helper functions

--- helperfunctions.c
+++ helperfunctions.c
@@ -8,11 +8,10 @@
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
  */
- void c_spec(va_list form_args, char *buffer, int *buff_it)
+void c_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
 	unsigned int i = *buff_it;
 
-	i++;
 	buffer[i] = (char)va_arg(form_args, int);
 	*buff_it = *buff_it + 1;
 }
@@ -22,8 +21,19 @@
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
  */
-void s_spec(va_list form_args, char *buffer, int *buff_it)
+void s_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
+	unsigned int i = *buff_it, j = 0;
+	char *str;
+
+	str = va_arg(form_args, char *);
+	while (str[j] != '\0')
+	{
+		buffer[i] = str[j];
+		i++;
+		j++;
+	}
+	*buff_it = *buff_it + j;
 }
 /**
  * d_spec - Will add a readable double value to the buffer printed
@@ -31,6 +41,14 @@ void s_spec(va_list form_args, char *buffer, int *buff_it)
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
  */
-void d_spec(va_list form_args, char *buffer, int *buff_it)
+void d_spec(va_list form_args, char *buffer, unsigned int *buff_it)
+{
+}
+/**
+ * @form_args: Format arguments
+ * @buffer: Buffer printed to stdout
+ * @buff_it: Pointer to buffer iterator
+ */
+void i_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
 }
