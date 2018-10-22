808a1871098ac9715f6f3eb3045505cb66a0c9fa
Andrew Lindburg <atlindburg@gmail.com>
2018-10-22 00:12:53 +0000

    Adding empty functions for helper functions so my code will compile. Also gonna create int to char* and double to char * conversion functions later

--- helperfunctions.c
+++ helperfunctions.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
- /**
+/**
  * c_spec - Will add a character to the buffer printed
  * @form_args: Format arguments
  * @buffer: Buffer printed to stdout
@@ -15,4 +15,22 @@
 	i++;
 	buffer[i] = (char)va_arg(form_args, int);
 	*buff_it = *buff_it + 1;
+}
+/**
+ * s_spec - Will add a string to the buffer printed
+ * @form_args: Format arguments
+ * @buffer: Buffer printed to stdout
+ * @buff_it: Pointer to buffer iterator
+ */
+void s_spec(va_list form_args, char *buffer, int *buff_it)
+{
+}
+/**
+ * d_spec - Will add a readable double value to the buffer printed
+ * @form_args: Format arguments
+ * @buffer: Buffer printed to stdout
+ * @buff_it: Pointer to buffer iterator
+ */
+void d_spec(va_list form_args, char *buffer, int *buff_it)
+{
 }
