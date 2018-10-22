14c835bfc667cf58b4fe1566344912c9f7d22c49
Athena Deng <539@holbertonschool.com>
2018-10-22 23:28:46 +0000

    first commit

---
+++ itoa.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: this string will be reversed
+ */
+void rev_string(char *s)
+{
+	int a;
+	int b;
+	int c;
+	int len;
+
+	a = 0;
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	b = len - 1;
+
+	while (a < b)
+	{
+		c = s[a];
+		s[a] = s[b];
+		s[b] = c;
+		a++;
+		b--;
+	}
+}
+
+/**
+ * itoa - converts integer into characters to be printed
+ * @n: integer to be converted
+ * @s: character string that integer will be converted into
+ */
+void itoa(int n, char *s)
+{
+	int i, o, n2;
+
+	i = 0;
+
+	if ((o = n) < 0)
+		n2 = (n * (-1));
+	else
+		n2 = n;
+	while ((n2 /= 10) > 0)
+		s[i++] = ((n2 % 10) + '0');
+	if (o < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+
+	rev_string(s);
+}
+
