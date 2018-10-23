a33a23767852127bc9a8f458c8a461d1af0c937d
Andrew Lindburg <atlindburg@gmail.com>
2018-10-23 00:29:02 +0000

    Character case is working

--- helperfunctions.c
+++ helperfunctions.c
@@ -7,21 +7,23 @@
  * @form_args: Format arguments
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
+ * Return: Buffer growth
  */
-void c_spec(va_list form_args, char *buffer, unsigned int *buff_it)
+unsigned int c_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
 	unsigned int i = *buff_it;
 
 	buffer[i] = (char)va_arg(form_args, int);
-	*buff_it = *buff_it + 1;
+	return (*buff_it + 1);
 }
 /**
  * s_spec - Will add a string to the buffer printed
  * @form_args: Format arguments
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
+ * Return: Buffer growth
  */
-void s_spec(va_list form_args, char *buffer, unsigned int *buff_it)
+unsigned int s_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
 	unsigned int i = *buff_it, j = 0;
 	char *str;
@@ -33,22 +35,49 @@ void s_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 		i++;
 		j++;
 	}
-	*buff_it = *buff_it + j;
+	return (i);
 }
 /**
  * d_spec - Will add a readable double value to the buffer printed
  * @form_args: Format arguments
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
+ * Return: Buffer growth
  */
-void d_spec(va_list form_args, char *buffer, unsigned int *buff_it)
+unsigned int d_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
+	unsigned int i = *buff_it, j = 0;
+	char *str;
+	double arg = va_arg(form_args, double);
+
+	str = null;
+	while (str[j] != '\0')
+	{
+		buffer[i] = str[j];
+		i++;
+		j++;
+	}
+	return (i);
 }
 /**
+ * i_spec - Will add a readble integer value to the buffer
  * @form_args: Format arguments
  * @buffer: Buffer printed to stdout
  * @buff_it: Pointer to buffer iterator
+ * Return: Buffer growth
  */
-void i_spec(va_list form_args, char *buffer, unsigned int *buff_it)
+unsigned int i_spec(va_list form_args, char *buffer, unsigned int *buff_it)
 {
+	unsigned int i = *buff_it, j = 0;
+	char *str;
+	int arg = va_arg(form_args, int);
+
+	str = null;
+	while (str[j] != '\0')
+	{
+		buffer[i] = str[j];
+		i++;
+		j++;
+	}
+	return (i);
 }
