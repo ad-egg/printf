3976b630af4a5a39f27d7bb902329f130622403b
Andrew Lindburg <atlindburg@gmail.com>
2018-10-23 00:24:36 +0000

    A lot of changes have been made. Basically consolidated code to a make_buffer and find_buffer_size function

--- holberton.h
+++ holberton.h
@@ -2,11 +2,14 @@
 #define HOLBERTON
 #include <stdlib.h>
 #include <stdarg.h>
+#define null (void *)0
 int _printf(const char *format, ...);
-void c_spec(va_list, char *buffer, unsigned int *buff_it);
-void s_spec(va_list, char *buffer, unsigned int *buff_it);
-void i_spec(va_list, char *buffer, unsigned int *buff_it);
-void d_spec(va_list, char *buffer, unsigned int *buff_it);
+char *make_buffer(va_list, const char *format, unsigned int *f_it);
+unsigned int find_buffer_size(char *buffer);
+unsigned int c_spec(va_list, char *buffer, unsigned int *buff_it);
+unsigned int s_spec(va_list, char *buffer, unsigned int *buff_it);
+unsigned int i_spec(va_list, char *buffer, unsigned int *buff_it);
+unsigned int d_spec(va_list, char *buffer, unsigned int *buff_it);
 /**
  * struct spec_finder - Stores format specifier and respective func
  * @spec: Format sepcifier
@@ -15,6 +18,6 @@ void d_spec(va_list, char *buffer, unsigned int *buff_it);
 typedef struct spec_finder
 {
 	char *spec;
-	void (*f)(va_list, char *, unsigned int *);
+	unsigned int (*f)(va_list, char *, unsigned int *);
 } find_t;
 #endif
